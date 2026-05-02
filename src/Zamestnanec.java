d14bc9d2eae1d6f4446bd3ee4b41d781a6217d78
Georgee <87183746+jirkamartinekk@users.noreply.github.com>
2026-05-02 12:01:23 +0200

    0.1

--- src/Zamestnanec.java
+++ src/Zamestnanec.java
@@ -4,37 +4,31 @@ public class Zamestnanec{
     private String prijmeni;
     private int rok_narozeni;
     private String skupina;
+    private static int counter = 0;
+
+    public Zamestnanec(String jmeno, String prijmeni, int rok_narozeni, String skupina) {
+        this.id = counter;
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.rok_narozeni = rok_narozeni;
+        this.skupina = skupina;
+        counter++;
+    }
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getJmeno() {
         return jmeno;
     }
 
-    public void setJmeno(String jmeno) {
-        this.jmeno = jmeno;
-    }
-
     public String getPrijmeni() {
         return prijmeni;
     }
 
-    public void setPrijmeni(String prijmeni) {
-        this.prijmeni = prijmeni;
-    }
-
     public int getRok_narozeni() {
         return rok_narozeni;
-    }
-
-    public void setRok_narozeni(int rok_narozeni) {
-        this.rok_narozeni = rok_narozeni;
     }
 
     public String getSkupina() {
@@ -43,13 +37,5 @@ public class Zamestnanec{
 
     public void setSkupina(String skupina) {
         System.out.println("Po vytvoreni zaznamu zamestnance jiz neni mozne jeho skupinu zmenit!");
-    }
-
-    public Zamestnanec(int id, String jmeno, String prijmeni, int rok_narozeni, String skupina) {
-        this.id = id;
-        this.jmeno = jmeno;
-        this.prijmeni = prijmeni;
-        this.rok_narozeni = rok_narozeni;
-        this.skupina = skupina;
     }
 }
