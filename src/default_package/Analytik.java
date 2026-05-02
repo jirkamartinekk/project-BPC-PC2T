7f641bebbdf48e56797c100bf59902236b832c6c
MikulasMarek <281773@vut.cz>
2026-05-02 13:10:12 +0200

    0.4

---
+++ src/default_package/Analytik.java
@@ -0,0 +1,14 @@
+package default_package;
+
+public class Analytik extends Zamestnanec{
+
+    public Analytik(String jmeno, String prijmeni, int rok_narozeni, String skupina){
+        super(jmeno, prijmeni, rok_narozeni, skupina);
+    }
+
+    public void spustDovednost(){
+        // napsat funkci pro vraceni spolupracovnika s nejvice spolecnymi spolupracovniky
+
+    }
+
+}
---
+++ src/default_package/Bezpecak.java
@@ -0,0 +1,13 @@
+package default_package;
+
+public class Bezpecak extends Zamestnanec{
+
+    public Bezpecak(String jmeno, String prijmeni, int rok_narozeni, String skupina){
+        super(jmeno, prijmeni, rok_narozeni, skupina);
+    }
+
+    public void spustDovednost(){
+        // napsat funkci pro vypocet prumeru urovne spoluprace
+
+    }
+}
